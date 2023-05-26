b30e337e6894476760660c341195b5718736d06e
Niraj Pradhan <niraj.pradhan.393@gmail.com>
2023-05-26 08:07:44 +0545

    add: blog interface

---
+++ src/@types/interface/blog.d.ts
@@ -0,0 +1,69 @@
+interface IBlog {
+  id: number;
+  date: string;
+  date_gmt: string;
+  guid: {
+    rendered: string;
+  };
+  modified: string;
+  modified_gmt: string;
+  slug: string;
+  status: string;
+  type: string;
+  link: string;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+  };
+  excerpt: {
+    rendered: string;
+    protected: boolean;
+  };
+  author: number;
+  featured_media: number;
+  comment_status: string;
+  ping_status: string;
+  sticky: boolean;
+  template: string;
+  format: string;
+  meta: {
+    _mi_skip_tracking: boolean;
+    _monsterinsights_sitenote_active: boolean;
+    _monsterinsights_sitenote_note: string;
+    _monsterinsights_sitenote_category: number;
+  };
+  categories: number[];
+  tags: any[];
+  yoast_head_json: {
+    title: string;
+    robots: {
+      index: string;
+      follow: string;
+      "max-snippet": string;
+      "max-image-preview": string;
+      "max-video-preview": string;
+    };
+    canonical: string;
+    og_locale: string;
+    og_type: string;
+    og_title: string;
+    og_description: string;
+    og_url: string;
+    og_site_name: string;
+    article_published_time: string;
+    og_image: {
+      width: number;
+      height: number;
+      url: string;
+      type: string;
+    }[];
+    author: string;
+    twitter_card: string;
+    twitter_misc: {
+      "Written by": string;
+      "Est. reading time": string;
+    };
+  };
+}
